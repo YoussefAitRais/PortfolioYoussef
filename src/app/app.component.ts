670392fc2269a9cca5d3184f379bda417b708aba
Youssef Ait Rais <copybuisness007@gmail.com>
2025-04-10 15:39:53 +0200

    project component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { AboutComponent } from "./about/about.component";
+import { ProjectsComponent } from "./projects/projects.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, NavbarComponent, AboutComponent],
+  imports: [RouterOutlet, HomeComponent, NavbarComponent, AboutComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
