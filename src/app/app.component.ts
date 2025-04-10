09d93f4e9355dc54a20ce9aaff8219380db5493a
Youssef Ait Rais <copybuisness007@gmail.com>
2025-04-10 16:11:49 +0200

    contact component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { AboutComponent } from "./about/about.component";
 import { ProjectsComponent } from "./projects/projects.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, NavbarComponent, AboutComponent, ProjectsComponent],
+  imports: [RouterOutlet, HomeComponent, NavbarComponent, AboutComponent, ProjectsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
