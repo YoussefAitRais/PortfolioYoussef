b9740c936b017f6b655d54f91afa93b4f969cdc4
Youssef Ait Rais <copybuisness007@gmail.com>
2025-04-11 16:09:30 +0200

    styling updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { AboutComponent } from "./about/about.component";
 import { ProjectsComponent } from "./projects/projects.component";
 import { ContactComponent } from "./contact/contact.component";
 import { FooterComponent } from "./footer/footer.component";
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, HomeComponent, NavbarComponent, AboutComponent, ProjectsComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,42 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
-export const routes: Routes = [];
+// 
+ 
+export const routes: Routes = [
+    {
+      path: '',
+    //   redirectTo: 'home', 
+    //   pathMatch: 'full'
+    component: HomeComponent
+    },
+    {
+      path: 'navbar',
+      component: NavbarComponent
+    },
+    {
+      path: 'home',
+      component: HomeComponent
+    },
+    {
+      path: 'about',
+      component: AboutComponent
+    },
+    {
+      path: 'projects',
+      component: ProjectsComponent
+    },
+    {
+      path: 'contact',
+      component: ContactComponent
+    },
+    {
+      path: 'footer',
+      component: FooterComponent
+    }
+];
